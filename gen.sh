469b2c95bd2950a98167c22c6733d836adc66d6e
Alexey Plenkin <aveplen@yandex.ru>
2022-05-18 20:57:19 +0300

    introduce directories

--- gen.sh
+++ gen.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 
+rm -rf facerec
+mkdir facerec
+
 protoc -I=. \
-    --go_out . \
+    --go_out facerec \
     --go_opt paths=source_relative \
-    --go-grpc_out . \
+    --go-grpc_out facerec \
     --go-grpc_opt paths=source_relative \
-    facerec.proto \
+    facerec.proto
+
+./venv/bin/python -m grpc_tools.protoc -I=.\
+    --python_out facerec \
+    --grpc_python_out facerec \
+    facerec.proto
+
+
+rm -rf s3g
+mkdir s3g
+
+protoc -I=. \
+    --go_out s3g \
+    --go_opt paths=source_relative \
+    --go-grpc_out s3g \
+    --go-grpc_opt paths=source_relative \
     s3g.proto
 
 ./venv/bin/python -m grpc_tools.protoc -I=.\
-    --python_out . \
-    --grpc_python_out . \
-    facerec.proto \
+    --python_out s3g \
+    --grpc_python_out s3g \
     s3g.proto
